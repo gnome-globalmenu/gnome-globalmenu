067841c1bcd7a9a739eaced98a82242a06a5da26
Yu Feng <rainwoodman@gmail.com>
2007-12-25 09:43:56 +0000

    added make patch script

---
+++ gtk2-aqd/patch.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+RPMDEVROOT=${RPMDEVROOT:?Define RPMDEVROOT before use}
+SRCRPM=gtk2*.src.rpm
+VER=`echo $SRCRPM |sed 's/[^-]*-\([^-]*\)-.*/\1/'`
+cp gtkmenubar.patch $RPMDEVROOT/SOURCES/
+(
+cd $RPMDEVROOT/BUILD/gtk+-$VER/gtk
+cp gtkmenubar.c.globalmenubar gtkmenubar.c
+patch -p1 -b -z .globalmenubar < ../../../SOURCES/gtkmenubar.patch
+make
+)
+rm -rf libs
+mkdir libs
+ln -s $RPMDEVROOT/BUILD/gtk+-$VER/gtk/.libs/* libs/
+cp $RPMDEVROOT/BUILD/gtk+-$VER/gtk/gtkmenubar.c.globalmenubar gtkmenubar.orig
+
--- gtk2-aqd/setup.sh
+++ gtk2-aqd/setup.sh
@@ -19,10 +19,13 @@ cp gtkmenubar.patch $RPMDEVROOT/SOURCES
 if [ ! -f $RPMDEVROOT/BUILD/gtk+-$VER/gtk/Makefile ]; then
 rpmbuild -bc --short-circuit gtk2-aqd.spec
 fi
-(cd $RPMDEVROOT/BUILD/gtk+-$VER/gtk; make)
+(
+cd $RPMDEVROOT/BUILD/gtk+-$VER/gtk
+make
+)
 rm -rf libs
 mkdir libs
 ln -s $RPMDEVROOT/BUILD/gtk+-$VER/gtk/.libs/* libs/
-cp $RPMDEVROOT/BUILD/gtk+-$VER/gtk/gtkmenubar.c .
 cp $RPMDEVROOT/BUILD/gtk+-$VER/gtk/gtkmenubar.c.globalmenubar gtkmenubar.orig
+cp $RPMDEVROOT/BUILD/gtk+-$VER/gtk/gtkmenubar.c gtkmenubar.c
 
