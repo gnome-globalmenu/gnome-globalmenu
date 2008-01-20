124746f8fa1ea336c527396d8ad95a530c892505
Yu Feng <rainwoodman@gmail.com>
2008-01-20 05:14:47 +0000

    Added gtk-doc comments.

--- libgnomenu/gdksocket.c
+++ libgnomenu/gdksocket.c
@@ -7,28 +7,36 @@
 #include "gdksocket-marshall.h"
 
 #define LOG_FUNC_NAME g_message(__func__)
+
+#define GDK_SOCKET_ATOM_STRING "GDK_SOCKET_MESSAGE"
+
 typedef struct _GdkSocketPrivate GdkSocketPrivate;
 
 struct _GdkSocketPrivate {
+	int foo;
 };
 
 #define GDK_SOCKET_GET_PRIVATE(obj) \
 	(G_TYPE_INSTANCE_GET_PRIVATE(obj, GDK_TYPE_SOCKET, GdkSocketPrivate))
 
 static GdkFilterReturn 
-	gdk_socket_window_filter_cb(GdkXEvent* xevent, GdkEvent * event, gpointer data);
+	gdk_socket_window_filter_cb			(GdkXEvent* xevent, GdkEvent * event, gpointer data);
+static void 
+	cleanup_data_arrival_signal_handler	(GdkSocket * socket, gpointer data, guint size);
 
 G_DEFINE_TYPE (GdkSocket, gdk_socket, G_TYPE_OBJECT)
 
-static void cleanup_data_arrival_signal_handler(GdkSocket * socket,
-	gpointer data, guint size){
-	g_free(data);
-	g_message("GdkSocket(%s)::%s ", socket->name, __func__);
-}
 static void
 gdk_socket_init(GdkSocket * self){
 	LOG_FUNC_NAME;
 }
+
+/**
+ * gdk_socket_new:
+ * @name: the name of the newly created socket, do not need to be unique.
+ *
+ * Returns: a newly created the #GdkSocket in #GDK_SOCKET_NEW state.
+ */
 GdkSocket *
 gdk_socket_new(char * name){
 	GdkWindowAttr attr;
@@ -51,17 +59,37 @@ gdk_socket_new(char * name){
 	gdk_window_add_filter(socket->window, gdk_socket_window_filter_cb, socket);
 	return socket;
 }
+/**
+ * gdk_socket_dispose:
+ * 	@object: The #GdkSocket to be disposed.
+ *
+ *  Should release all of the ref counts and set a disposed state of the object
+ *  TODO: unref all the resources and set state to #GDK_SOCKET_DISPOSED.
+ */
 static void
 gdk_socket_dispose(GObject * object){
-	/*Should release all of the ref counts and set a disposed state of the object*/
-	/*However don't use this function yet*/
+	GdkSocket * self = GDK_SOCKET(object);
+	LOG_FUNC_NAME;
+	G_OBJECT_CLASS(gdk_socket_parent_class)->dispose(object);
 }
+/**
+ * gdk_socket_finalize:
+ * @object: the #GdkSocket to be finalized.
+ *
+ *  free all the resoursed occupied by @object
+ **/
 static void
 gdk_socket_finalize(GObject * object){
 	GdkSocket * self = GDK_SOCKET(object);
 	LOG_FUNC_NAME;
 	g_free(self->name);
+	G_OBJECT_CLASS(gdk_socket_parent_class)->finalize(object);
 }
+/**
+ * gdk_socket_class_init:
+ *
+ * Initialize the class structure of #GdkSocket
+ */
 static void
 gdk_socket_class_init(GdkSocketClass * klass){
 	GObjectClass * gobject_class = G_OBJECT_CLASS(klass);
@@ -69,10 +97,21 @@ gdk_socket_class_init(GdkSocketClass * klass){
 
 	g_type_class_add_private(gobject_class, sizeof (GdkSocketPrivate));
 
+	gobject_class->dispose = gdk_socket_dispose;
 	gobject_class->finalize = gdk_socket_finalize;
 
 	klass->data_arrival_cleanup = cleanup_data_arrival_signal_handler;
 	klass->data_arrival_signal_id = 
+/**
+ * GdkSocket::data-arrival:
+ * @self: the #GdkSocket that receives this signal.
+ * @data: the received data. It is owned by @self and the signal handler 
+ * 		should not free it.
+ * @bytes: the length of received data.
+ *
+ * The ::data-arrival signal is emitted each time a message arrives to
+ * the socket.
+ */
 		g_signal_new ("data-arrival",
 			G_TYPE_FROM_CLASS (klass),
 			G_SIGNAL_RUN_CLEANUP | G_SIGNAL_NO_RECURSE | G_SIGNAL_NO_HOOKS,
@@ -86,9 +125,34 @@ gdk_socket_class_init(GdkSocketClass * klass){
 			G_TYPE_UINT
 			);
 }
+
+/**
+ * gdk_socket_get_native:
+ * 	@self: the #GdkSocket this method acts on.
+ *
+ * Returns: the native id of this socket. It happens to be the Window ID of the
+ * 	@GdkWindow the GdkSocket wraps, in current implement.
+ */
 GdkNativeWindow gdk_socket_get_native(GdkSocket * self){
 	return GDK_WINDOW_XWINDOW(self->window);
 }
+
+/**
+ * gdk_socket_send:
+ * @self: the #GdkSocket this method acts on.
+ * @target: the native window id of target socket. In current implement, this
+ * 		identifies the native window id of #GdkSocket::window.
+ * @data: the data buffer. After calling this function, the buffer can be freed.
+ * @bytes: the length of data wanted to send.
+ *
+ * This method of #GdkSocket sends a messange to the target socket whose id 
+ * is @target. Whether or not the target is a GdkSocket is not checked
+ * (and impossible to check). The length of data has is limited by XClientMessage
+ * to be 20 bytes. (or even fewer if in the future the socket adds a header to the
+ * message.
+ *
+ * Returns: if successful, return TRUE else return FALSE.
+ */
 gboolean gdk_socket_send(GdkSocket * self, GdkNativeWindow target, gpointer data, guint bytes){
 #ifndef GDK_WINDOWING_X11
 #error ONLY X11 is supported. other targets are not yet supported.
@@ -111,7 +175,7 @@ gboolean gdk_socket_send(GdkSocket * self, GdkNativeWindow target, gpointer data
           target,
           False, NoEventMask, (XEvent *)&xclient);
     gdk_display_sync (self->display); /*resolve the message*/
-    gdk_error_trap_pop ();
+    return gdk_error_trap_pop () == 0;
 }
 
 static GdkFilterReturn 
@@ -136,4 +200,10 @@ static GdkFilterReturn
         }
 	} 
 	return GDK_FILTER_CONTINUE;
+}
+
+static void cleanup_data_arrival_signal_handler(GdkSocket * socket,
+	gpointer data, guint size){
+	g_free(data);
+	g_message("GdkSocket(%s)::%s ", socket->name, __func__);
 }
--- libgnomenu/gdksocket.h
+++ libgnomenu/gdksocket.h
@@ -2,6 +2,18 @@
 #ifndef GDK_SOCKET_H
 #define GDK_SOCKET_H
 
+G_BEGIN_DECLS
+
+/**
+ * SECTION: gdksocket
+ * @short_description: Connectionless socket for GTK.
+ * @see_also: #GdkWindow, #GtkGlobalMenuBar
+ * @stability: unstable, development
+ * @include: libgnomenu/gdksocket.h
+ *
+ * GdkSocket handles inter-process communication of GTK applications. 
+ * It is the fundanmental communication mechanism for #libgnomenu.
+ */
 
 #define GDK_TYPE_SOCKET 	(gdk_socket_get_type())
 #define GDK_SOCKET(obj)		(G_TYPE_CHECK_INSTANCE_CAST((obj), GDK_TYPE_SOCKET, GdkSocket))
@@ -10,35 +22,62 @@
 #define GDK_IS_SOCKET_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GDK_TYPE_SOCKET))
 #define GDK_SOCKET_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS((obj), GDK_TYPE_SOCKET, GdkSocketClass))
 
-typedef struct _GdkSocket GdkSocket;
-typedef struct _GdkSocketClass GdkSocketClass;
 
-typedef enum _GdkSocketStatus {
+typedef struct _GdkSocketClass GdkSocketClass;
+typedef struct _GdkSocket GdkSocket;
+
+/** 
+ * GdkSocketStatus:
+ *   @GDK_SOCKET_NEW: A newly created GdkSocket, the associated GdkWindow is created.
+ *   @GDK_SOCKET_SHUTDOWN: The socket was shutdown. LACK DEFINITION.
+ *   @GDK_SOCKET_DISPOSED: The socket was disposed. Can not receive or
+ *   			send any message
+ */
+typedef enum {
 	GDK_SOCKET_NEW,
 	GDK_SOCKET_SHUTDOWN,
-	GDK_SOCKET_DISPOSED,
+	GDK_SOCKET_DISPOSED
 } GdkSocketStatus;
 
+/**
+ * GdkSocket:
+ * 	@name:	name of the socket.
+ * 	@window: the #GdkWindow used to receive messages. Its #GdkWindow::title will be @name.
+ * 	@display: the #GdkDisplay this socket belongs to. Though we can always obtain 
+ * 		this information from @window, we cache it here for saving code lines.
+ * 	@status: the status. See #GdkSocketStatus.
+ *
+ *  The GdkSocket object.
+ */
 struct _GdkSocket {
 	GObject parent;
+/*< public >*/
 	gchar * name;
 	GdkWindow * window;
 	GdkDisplay * display;
 	GdkSocketStatus status;
 };
 
+/**
+ * GdkSocketClass:
+ *   @data_arrival_signal_id: the signal id for ::data-arrival.
+ *   @data_arrival_cleanup: the cleanup call back for ::data-arrival signal
+ */
 struct _GdkSocketClass {
 	GObjectClass parent;
+/* < private >*/
 	guint data_arrival_signal_id;
 	void (*data_arrival_cleanup) (GdkSocket * self, gpointer data, guint length);
 };
+
 GType gdk_socket_get_type (void);
+
 GdkSocket * gdk_socket_new (gchar * name);
-gboolean gdk_socket_send(GdkSocket * self, 
-	GdkNativeWindow target, 
-	gpointer data, 
-	guint bytes);
+
+gboolean gdk_socket_send(GdkSocket * self, GdkNativeWindow target, gpointer data, guint bytes);
+
 GdkNativeWindow gdk_socket_get_native(GdkSocket * self);
-#define GDK_SOCKET_ATOM_STRING "GDK_SOCKET_MESSAGE"
+
+G_END_DECLS
 #endif
 
