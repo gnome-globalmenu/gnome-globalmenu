8693f09412c8f57b805b11ff6eb52c0061af4632
Yu Feng <rainwoodman@gmail.com>
2008-02-21 07:38:26 +0000

    I give up code reusing. remove everything and let's have a new start. The old code is so messy.

--- gnome-globalmenu-applet/application.h
+++ gnome-globalmenu-applet/application.h
@@ -1,29 +1,6 @@
-struct _WnckScreen;
-struct _ClientInfo;
-struct _Application {
-	GtkContainer * MainWindow;
-	struct _MenuServer * Server;
-	struct {
-		GtkImage * ClientIcon;
-		GtkLabel * TitleLabel;
-		GtkFixed * Holder; /*since it is the only widget i know who can has a window*/
-		GtkEventBox * Backward;
-		GtkEventBox * Forward;
-	}; /*Widgets*/
-	struct {
-		GdkPixmap * Background;
-		GdkColor Color;
-		gboolean show_title; /*If show current window's title*/
-		gboolean show_icon; /*current window's icon*/
-/*The above two property has no effect, since not implemented yet.*/
-		gboolean show_arrows; /*whether show the stupid scrolling arrows*/
-		gint title_max_width_chars; /*max width of the label in chars*/
-	} AppletProperty;
-	struct _WnckScreen * Screen;
-	struct _ClientInfo * ActiveClient;
-	gchar * ActiveTitle;
-	GdkPixbuf * ActiveIcon;
-	GList * Clients;
-};
+typedef struct _Application {
+	GtkContainer * window;
+	GnomenuServerHelper * helper;
+	WnckScreen * screen;
+} Application;
 
-typedef struct _Application Application;
