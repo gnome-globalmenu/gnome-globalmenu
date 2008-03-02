6bd787e15fdc0dc07a4685853cd9c58ed3861196
Yu Feng <rainwoodman@gmail.com>
2008-03-02 22:01:51 +0000

    rearrange application.c.

--- servers/application.c
+++ servers/application.c
@@ -27,17 +27,76 @@ typedef struct _ApplicationPrivate {
 static void _s_window_destroy(Application * app, GtkWidget * widget);
 static void _s_active_client_changed(Application * app, MenuServer * server);
 static void _s_menu_bar_area_size_allocate(Application * app, GtkAllocation * allocation, GtkWidget * widget);
-static void _s_conf_dialog_response(Application * self, gint arg, GtkWidget * dialog);
+static void _s_conf_dialog_response(Application * app, gint arg, GtkWidget * dialog);
 
-static void _update_background(Application * app);
+/* Application Interface */
+static void _update_ui					(Application *app);
+static void _save_conf_unimp			(Application *app);
+static void _load_conf_unimp			(Application *app);
 
-static GObject *_constructor( GType type, guint n_construct_properties,
-				  GObjectConstructParam * construct_params) ;
+/* GObject Interface */
+static void _set_property				( GObject * obj, guint property_id, 
+										  const GValue * value, GParamSpec * pspec);
+static void _get_property				( GObject * obj, guint property_id, 
+										  GValue * value, GParamSpec * pspec);
+static void _finalize					( GObject *obj);
+static GObject *_constructor			( GType type, guint n_construct_properties,
+										  GObjectConstructParam * construct_params) ;
 
+/* tool function*/
+static void _update_background			(Application * app);
 
 
 G_DEFINE_TYPE		(Application, application, G_TYPE_OBJECT);
 
+static void application_init(Application *app)
+{
+}
+static void application_class_init(ApplicationClass *klass)
+{
+	GObjectClass *obj_class = G_OBJECT_CLASS(klass);
+	
+	obj_class->constructor = _constructor;
+	obj_class->finalize = _finalize;
+	obj_class->set_property = _set_property;
+	obj_class->get_property = _get_property;
+
+	klass->update_ui = _update_ui;
+	klass->load_conf = _load_conf_unimp;
+	klass->save_conf = _save_conf_unimp;
+
+	g_object_class_install_property (obj_class,
+		PROP_WINDOW,
+		g_param_spec_object ("window",
+						"window",
+						"applet window",
+						GTK_TYPE_WIDGET,
+						G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE));
+
+	g_object_class_install_property (obj_class,
+		PROP_TITLE_VISIBLE,
+		g_param_spec_boolean ("title-visible",
+						"title-visible",
+						"whether or not display the title",
+						FALSE,
+						G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
+
+	g_object_class_install_property (obj_class,
+		PROP_ICON_VISIBLE,
+		g_param_spec_boolean ("icon-visible",
+						"icon-visible",
+						"whether or not display the title",
+						FALSE,
+						G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
+
+	g_type_class_add_private(obj_class, sizeof(ApplicationPrivate));
+}
+
+Application * application_new(GtkContainer * widget){
+	return g_object_new(TYPE_APPLICATION, "window", widget, NULL);
+}
+
+/* BEGINS: Application Interface */
 static void _update_ui(Application *app)
 {
 	LOG();
@@ -64,8 +123,9 @@ static void _load_conf_unimp(Application *app)
 	LOG("Not implemented for %s\n", 
 			g_type_name(G_TYPE_FROM_INSTANCE(app)));
 }
+/* ENDS: Application Interface */
 
-
+/* BEGINS: GObject Interface */
 static void 
 _set_property( GObject * _self, guint property_id, const GValue * value, GParamSpec * pspec){
 
@@ -114,10 +174,6 @@ _get_property( GObject * _self, guint property_id, GValue * value, GParamSpec * 
 }
 
 
-static void application_init(Application *app)
-{
-}
-
 static void _finalize(GObject *obj)
 {
 	Application *app = APPLICATION(obj);
@@ -126,48 +182,6 @@ static void _finalize(GObject *obj)
 		g_object_unref(app->window);
 }
 
-static void application_class_init(ApplicationClass *klass)
-{
-	GObjectClass *obj_class = G_OBJECT_CLASS(klass);
-	
-	obj_class->constructor = _constructor;
-	obj_class->finalize = _finalize;
-	obj_class->set_property = _set_property;
-	obj_class->get_property = _get_property;
-
-	klass->update_ui = _update_ui;
-	klass->load_conf = _load_conf_unimp;
-	klass->save_conf = _save_conf_unimp;
-
-	g_object_class_install_property (obj_class,
-		PROP_WINDOW,
-		g_param_spec_object ("window",
-						"window",
-						"applet window",
-						GTK_TYPE_WIDGET,
-						G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE));
-
-	g_object_class_install_property (obj_class,
-		PROP_TITLE_VISIBLE,
-		g_param_spec_boolean ("title-visible",
-						"title-visible",
-						"whether or not display the title",
-						FALSE,
-						G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
-
-	g_object_class_install_property (obj_class,
-		PROP_ICON_VISIBLE,
-		g_param_spec_boolean ("icon-visible",
-						"icon-visible",
-						"whether or not display the title",
-						FALSE,
-						G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
-
-	g_type_class_add_private(obj_class, sizeof(ApplicationPrivate));
-}
-Application * application_new(GtkContainer * widget){
-	return g_object_new(TYPE_APPLICATION, "window", widget, NULL);
-}
 static GObject * 
 _constructor	( GType type, guint n_construct_properties,
 				  GObjectConstructParam * construct_params) {
@@ -238,7 +252,9 @@ _constructor	( GType type, guint n_construct_properties,
 
 	return _self;
 }
+/* ENDS: GObject Interface */
 
+/* Public methods */
 void application_set_background(Application * app, GdkColor * color, GdkPixmap * pixmap){
 	gboolean dirty = FALSE;
 /* This piece code is redundant, for the purpose of clearity*/
@@ -279,64 +295,6 @@ void application_set_background(Application * app, GdkColor * color, GdkPixmap *
 		_update_background(app);
 }
 
-static void _update_background(Application * app){
-	GdkPixmap * cropped = NULL;  /*clear bg by default*/
-	GdkGC * gc;
-	GtkAllocation * a;
-	GdkPixmap * pixmap = app->bgpixmap;
-	GdkColor * color = app->bgcolor;
-	a = &GTK_WIDGET(app->menu_bar_area)->allocation;
-	LOG();
-	if (pixmap) { /* get the cropped pixmap for menu bar area*/
-		cropped = gdk_pixmap_new(pixmap, a->width, a->height, -1);
-		gc = gdk_gc_new(pixmap);
-		gdk_draw_drawable(cropped, gc, pixmap, a->x, a->y, 0, 0, a->width, a->height);
-		g_object_unref(gc);
-	} 
-
-	g_object_set(app->server, "bg-color", color, 
-							"bg-pixmap", cropped, NULL);
-	g_object_unref(cropped);
-	utils_set_widget_background(app->menu_bar_area, color, cropped);	
-}
-
-static void _s_active_client_changed(Application * app, MenuServer * server){
-	ApplicationClass *app_class = G_OBJECT_GET_CLASS(app);
-	GdkPixbuf *icon_buf, *resized_icon;
-	gint w, h;
-	WnckWindow * window = menu_server_get_client_parent(server, server->active);
-
-	if(!window) window = wnck_screen_get_active_window(wnck_screen_get_default());
-	if(!window) return;
-	WnckApplication * application = wnck_window_get_application(window);
-
-	const gchar *name = wnck_application_get_name(application);
-	gtk_label_set_text(app->title, name);
-
-	icon_buf =  wnck_application_get_icon(application);
-	if (icon_buf) {
-		/* FIXME : check the direction fisrt? */
-		w = h = GTK_WIDGET(app->window)->allocation.height ;
-		resized_icon = gdk_pixbuf_scale_simple(icon_buf , w, h, GDK_INTERP_BILINEAR);
-		gtk_image_set_from_pixbuf(app->icon, resized_icon);
-		g_object_unref(resized_icon);
-	}
-	application_update_ui(app);
-}
-
-static void _s_window_destroy(Application * app, GtkWidget * widget){
-	LOG();
-	g_object_unref(G_OBJECT(app));
-}
-static void _s_menu_bar_area_size_allocate(Application * app, GtkAllocation * allocation, GtkWidget * widget){
-	static GtkAllocation old_allo = {0};
-	LOG("+%d,%d,%d,%d", *allocation);
-	LOG("-%d,%d,%d,%d", old_allo);
-	if(memcmp(&old_allo, allocation, sizeof(GtkAllocation)))
-		_update_background(app);	
-	old_allo = *allocation;
-}
-
 void application_update_ui(Application *app){
 	if(APPLICATION_GET_CLASS(app)->update_ui){
 		APPLICATION_GET_CLASS(app)->update_ui(app);
@@ -373,6 +331,45 @@ void application_show_about_dialog(Application * app){
 	gtk_show_about_dialog(NULL, 
 				"authors", authors, NULL);
 }
+/* END: Public Methods */
+
+/* BEGIN: Signal handlers */
+static void _s_active_client_changed(Application * app, MenuServer * server){
+	ApplicationClass *app_class = G_OBJECT_GET_CLASS(app);
+	GdkPixbuf *icon_buf, *resized_icon;
+	gint w, h;
+	WnckWindow * window = menu_server_get_client_parent(server, server->active);
+
+	if(!window) window = wnck_screen_get_active_window(wnck_screen_get_default());
+	if(!window) return;
+	WnckApplication * application = wnck_window_get_application(window);
+
+	const gchar *name = wnck_application_get_name(application);
+	gtk_label_set_text(app->title, name);
+
+	icon_buf =  wnck_application_get_icon(application);
+	if (icon_buf) {
+		/* FIXME : check the direction fisrt? */
+		w = h = GTK_WIDGET(app->window)->allocation.height ;
+		resized_icon = gdk_pixbuf_scale_simple(icon_buf , w, h, GDK_INTERP_BILINEAR);
+		gtk_image_set_from_pixbuf(app->icon, resized_icon);
+		g_object_unref(resized_icon);
+	}
+	application_update_ui(app);
+}
+
+static void _s_window_destroy(Application * app, GtkWidget * widget){
+	LOG();
+	g_object_unref(G_OBJECT(app));
+}
+static void _s_menu_bar_area_size_allocate(Application * app, GtkAllocation * allocation, GtkWidget * widget){
+	static GtkAllocation old_allo = {0};
+	LOG("+%d,%d,%d,%d", *allocation);
+	LOG("-%d,%d,%d,%d", old_allo);
+	if(memcmp(&old_allo, allocation, sizeof(GtkAllocation)))
+		_update_background(app);	
+	old_allo = *allocation;
+}
 static void _s_conf_dialog_response(Application * self, gint arg, GtkWidget * dialog){
 	Application * app = APPLICATION(self);
 
@@ -394,6 +391,30 @@ static void _s_conf_dialog_response(Application * self, gint arg, GtkWidget * di
 	}
 	gtk_widget_hide(dialog);
 }
+/* END: Signal handlers */
+
+/* BEGIN: tool functions*/
+static void _update_background(Application * app){
+	GdkPixmap * cropped = NULL;  /*clear bg by default*/
+	GdkGC * gc;
+	GtkAllocation * a;
+	GdkPixmap * pixmap = app->bgpixmap;
+	GdkColor * color = app->bgcolor;
+	a = &GTK_WIDGET(app->menu_bar_area)->allocation;
+	LOG();
+	if (pixmap) { /* get the cropped pixmap for menu bar area*/
+		cropped = gdk_pixmap_new(pixmap, a->width, a->height, -1);
+		gc = gdk_gc_new(pixmap);
+		gdk_draw_drawable(cropped, gc, pixmap, a->x, a->y, 0, 0, a->width, a->height);
+		g_object_unref(gc);
+	} 
+
+	g_object_set(app->server, "bg-color", color, 
+							"bg-pixmap", cropped, NULL);
+	g_object_unref(cropped);
+	utils_set_widget_background(app->menu_bar_area, color, cropped);	
+}
+/* END: tool functions*/
 /*
 vim:ts=4:sw=4
 */
