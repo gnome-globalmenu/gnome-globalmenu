866ca7dfd9a6b791e3e60a75e5081ad969e94bbb
Yu Feng <rainwoodman@gmail.com>
2008-03-15 06:43:08 +0000

    skip invoking configure --enable-maintainer-mode . Warning (panic) if you make install for --enable-maintainer-mode.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+NOCONFIGURE=yes USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
 
 exit;
 srcdir=`dirname $0`
