cf94e594983c12203ad88cd45032bd8cf4ef561d
Yu Feng <rainwoodman@gmail.com>
2008-05-25 05:10:19 +0000

    add missing files

---
+++ libgnomenu/widget.c
@@ -0,0 +1,17 @@
+#include <config.h>
+#include <gtk/gtk.h>
+#include "introspector.h"
+
+gchar * gtk_widget_get_id(GtkWidget * widget){
+	return g_object_get_data(widget, "introspect-id");
+}
+void gtk_widget_set_id(GtkWidget * widget, gchar * id){
+		g_object_set_data_full(widget, "introspect-id", g_strdup(id), g_free);
+}
+gchar * gtk_widget_introspect(GtkWidget * widget){
+	gchar * rt;
+	Introspector * spector = introspector_new();
+	introspector_queue_widget(spector, widget);
+	introspector_visit_all(spector);
+	return introspector_destroy(spector, FALSE);
+}
---
+++ libgnomenu/widget.h
@@ -0,0 +1,3 @@
+gchar * gtk_widget_introspect(GtkWidget * widget);
+void gtk_widget_set_id(GtkWidget * widget, gchar * id);
+gchar * gtk_widget_get_id(GtkWidget * widget);
