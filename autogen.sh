6b6c8dc17c56ba53104182182b9af569f36c61d8
Yu Feng <rainwoodman@gmail.com>
2008-07-24 19:39:40 +0000

    Divided the servers directory into xfce and gnome.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh --enable-gtk-doc
 
 exit;
 srcdir=`dirname $0`
---
+++ gnome-applet/log.h
@@ -0,0 +1,9 @@
+#include <config.h>
+#if ENABLE_TRACING >= 1
+#define LOG(fmt, args...) g_message("%s::%s:" fmt, __FILE__, __func__, ## args)
+#else
+#define LOG(fmt, args...) 
+#endif
+/*
+vim:ts=4:sw=4
+*/
