78b8508e2eaa6c38da996e887bd223a113f65002
Yu Feng <rainwoodman@gmail.com>
2008-07-27 03:06:44 +0000

    added method names

--- libgnomenu/server/main.c
+++ libgnomenu/server/main.c
@@ -30,6 +30,12 @@ int main(int argc, char* argv[]){
 
 	client_hash = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
 	ipc_server_register_cmd("Ping", ping, NULL);
+	ipc_server_register_cmd("CreateObject", ping, NULL);
+	ipc_server_register_cmd("DestroyObject", ping, NULL);
+	ipc_server_register_cmd("SetProperty", ping, NULL);
+	ipc_server_register_cmd("ActivateObject", ping, NULL);
+	ipc_server_register_cmd("InsertChild", ping, NULL);
+	ipc_server_register_cmd("RemoveChild", ping, NULL);
 	if(!ipc_server_listen(client_create_callback, client_destroy_callback, NULL)) {
 		g_error("server already there");
 		return 1;
