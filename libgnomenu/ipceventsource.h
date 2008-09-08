d0496b8aa8d82cc8a1db65c0e0e1c302e1749936
Yu Feng <rainwoodman@gmail.com>
2008-09-08 04:06:14 +0000

    draft for ipcevent source. no coding yet.

---
+++ libgnomenu/ipceventsource.h
@@ -0,0 +1,16 @@
+#ifndef _IPC_EVENT_SOURCE_H_
+#define _IPC_EVENT_SOURCE_H_
+#include "ipcevent.h"
+#define IPC_EVENT_SOURCE_REGISTER(name, in) \
+{ \
+	static gchar * _ipc_in = in; \
+	ipc_event_source_register(event, in); \
+}
+void ipc_event_source_register(const gchar * event, const gchar * in);
+void ipc_event_source_mute(const gchar * event);
+void ipc_event_source_unmute(const gchar * event);
+void ipc_event_source_emit(const gchar * event, ...);
+void ipc_event_source_emit_va(const gchar * event, va_list va);
+void ipc_event_source_emit_array(const gchar * event, gchar ** parameters, gchar ** values);
+void ipc_event_source_emit_event(IPCEvent * event);
+#endif
