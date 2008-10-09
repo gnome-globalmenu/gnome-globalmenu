be300ebe8886345a452571c312516d0331a2e219
Yu Feng <rainwoodman@gmail.com>
2008-10-09 14:45:39 +0000

    Remove offsprings of the merging

--- libgnomenu/binding-gtk.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MENU_H_
-#define _MENU_H_
-void gnomenu_disable();
-gboolean gnomenu_init();
-void gnomenu_wrap_widget(GtkWidget * widget);
-void gnomenu_bind_menu(GdkWindow * window, GtkWidget * menu);
-void gnomenu_unbind_menu(GdkWindow * window, GtkWidget * menu);
-#endif
--- libgnomenu/ipceventsink.h
+++
@@ -1,8 +0,0 @@
-#ifndef _IPC_EVENT_SINK_H_
-#define _IPC_EVENT_SINK_H_
-#include "ipcevent.h"
-typedef void (*IPCEventHandler)(IPCEvent * event, gpointer data);
-void ipc_event_sink_listen(const gchar * event, const gchar * source, IPCEventHandler handler, gpointer data);
-void ipc_event_sink_unlisten(const gchar * event, const gchar * source, IPCEventHandler handler, gpointer data);
-void ipc_event_sink_dispatch(IPCEvent * event);
-#endif
