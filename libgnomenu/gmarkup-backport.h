ecf5ebd959e5c425eb94633ada99ebb0df21e0ac
Yu Feng <rainwoodman@gmail.com>
2009-01-15 04:54:41 +0000

    Add the backport of g_markup_collect_values; prepare for GLIB 2.12 build.

---
+++ libgnomenu/gmarkup-backport.h
@@ -0,0 +1,27 @@
+#include <glib.h>
+#if GLIB_MINOR_VERSION < 16
+#include <stdarg.h>
+G_BEGIN_DECLS
+typedef enum
+{
+  G_MARKUP_COLLECT_INVALID,
+  G_MARKUP_COLLECT_STRING,
+  G_MARKUP_COLLECT_STRDUP,
+  G_MARKUP_COLLECT_BOOLEAN,
+  G_MARKUP_COLLECT_TRISTATE,
+
+  G_MARKUP_COLLECT_OPTIONAL = (1 << 16)
+} GMarkupCollectType;
+
+
+/* useful from start_element */
+gboolean   g_markup_collect_attributes (const gchar         *element_name,
+                                        const gchar        **attribute_names,
+                                        const gchar        **attribute_values,
+                                        GError             **error,
+                                        GMarkupCollectType   first_type,
+                                        const gchar         *first_attr,
+                                        ...);
+
+G_END_DECLS
+#endif
