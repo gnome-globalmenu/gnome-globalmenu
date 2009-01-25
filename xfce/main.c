dfbe3ccb2c471eb32927a0daab6e72751067b791
Yu Feng <rainwoodman@gmail.com>
2009-01-25 23:32:18 +0000

    update to new monitor

--- xfce/main.c
+++ xfce/main.c
@@ -1,5 +1,5 @@
 #include <gtk/gtk.h>
-#include "libgnomenu/menubar.h"
+#include "libgnomenu/globalmenu.h"
 #include "applet/monitor.h"
 
 #include <libxfce4panel/xfce-panel-plugin.h>
@@ -27,23 +27,25 @@ static void xfce_panel_plugin_init (XfcePanelPlugin * plugin) {
 	);
 	gtk_widget_reset_rc_styles(plugin);
 	GnomenuMonitor * monitor = gnomenu_monitor_new();
-	GnomenuMenuBar * menubar = gnomenu_menu_bar_new();
+	GnomenuGlobalMenu * menubar = gnomenu_global_menu_new();
 	g_object_ref_sink(menubar);
 	g_object_set_data_full(G_OBJECT(plugin), "monitor", monitor, g_object_unref);
+	g_object_set_data_full(G_OBJECT(plugin), "menubar", menubar, g_object_unref);
 
 	xfce_panel_plugin_add_action_widget(plugin, GTK_WIDGET(plugin));
 	xfce_panel_plugin_set_expand(plugin, TRUE);
 
-	gnomenu_monitor_set_menubar(monitor, menubar);
 	xfce_panel_plugin_add_action_widget(plugin, GTK_WIDGET(menubar));
 
 	gtk_container_add(GTK_CONTAINER(plugin), GTK_WIDGET(menubar));
 	g_signal_connect(monitor, "window-changed", on_window_changed, plugin);
 	gtk_widget_show(menubar);
-	g_object_unref(menubar);
 
 }
 
 static void on_window_changed(GnomenuMonitor * monitor, WnckWindow * prev_window, XfcePanelPlugin * plugin) {
-	g_message("window changed to %p", gnomenu_monitor_get_current_window);
+	WnckWindow * current_window = gnomenu_monitor_get_current_window(monitor);
+	GnomenuGlobalMenu * menubar = g_object_get_data(G_OBJECT(plugin),
+			"menubar");
+	gnomenu_global_menu_switch_to(menubar, wnck_window_get_xid(current_window));
 }
