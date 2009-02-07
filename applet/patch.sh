51ff80cdb7ade89a5402c5a7445c6764a46a4500
Yu Feng <rainwoodman@gmail.com>
2009-02-07 17:47:29 +0000

    workaround the broken header dependency scheme in vala 0.5.6, solves issue 20.

--- applet/patch.sh
+++ applet/patch.sh
@@ -4,4 +4,4 @@ source ../patch.sh
 
 patch '/g_return_if_fail (previous_screen != NULL)/d' applet.c
 patch '/gnome_program_init/s/APPLET_STANDARD_PROPERTIES/GNOME_PROGRAM_STANDARD_PROPERTIES/' main.c
-
+patch '/stdlib.h/i#include <glib/gi18n-lib.h>' main.h
