d51ad2a41fe99e299167af7bcea1186cbd4d505a
Yu Feng <rainwoodman@gmail.com>
2009-02-08 16:01:22 +0000

    optimize libgnomenu building process.

--- libgnomenu/patch.sh
+++ libgnomenu/patch.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 source ../patch.sh
+if [ $1 == 'backup' ]; then
+backup menuitem.c globalmenu.c monitor.c menubar.c menubarbox.c label.c
+exit 0;
+fi;
 
 patch '/g_return_if_fail.*old_parent/d' menuitem.c
 patch '/g_return_if_fail.*previous_screen/d' menuitem.c globalmenu.c
@@ -9,3 +13,5 @@ patch '/g_return_if_fail (previous_window != NULL)/d' monitor.c
 patch 's/gboolean include_internals;//g;s/include_internals = FALSE;//g' menuitem.c menubar.c menubarbox.c label.c
 patch 's/GtkCallback callback, void\* callback_target, void\* data/gboolean include_internals, GtkCallback callback, void* callback_target/g' menuitem.c menubar.c menubarbox.c label.c
 patch 's/callback, callback_target, data/include_internals, callback, callback_target/g' menuitem.c menubar.c
+
+check_restore menuitem.c globalmenu.c monitor.c menubar.c menubarbox.c label.c
