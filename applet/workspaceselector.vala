615a7b37e897bb5043207c2b28390100948840ad
Luca Viggiani <lviggiani@gmail.com>
2009-02-14 13:12:29 +0000

    Now get_mini_screenshot returns the Desktop background image. TODO: render thumbnails of windows on the background

--- applet/workspaceselector.vala
+++ applet/workspaceselector.vala
@@ -2,6 +2,16 @@ using GLib;
 using Gtk;
 using Wnck;
 
+extern Gdk.Pixbuf*
+_wnck_gdk_pixbuf_get_from_pixmap (Gdk.Pixbuf*   dest,
+                                  Gdk.Pixmap*   xpixmap,
+                                  int          src_x,
+                                  int          src_y,
+                                  int          dest_x,
+                                  int          dest_y,
+                                  int          width,
+                                  int          height);
+                                  
 class WorkspaceSelector : Gtk.Window {
 	private Wnck.Window? target;
 	private const int WIDTH = 128;
@@ -71,37 +81,27 @@ class WorkspaceSelector : Gtk.Window {
 		this.show_all();
 	}
 	private Gdk.Pixbuf get_mini_screenshot(int nth_vdesktop) {
-		/* TOFIX:
-		 * This will always return what is being shown on the screen
-		 * which is not a true screen capture of the Desktop window
-		 * but what is actually being rendered (i.e. the Desktop cube rotating).
-		 * This function should return the actual screen show of selected 
-		 * virtual Desktop (i.e. Compiz cube face) */
+		/* TODO:
+		 * Render also windows in selected Desktop */
+
+		Gdk.Pixmap *p = (Gdk.Pixmap*)target.get_screen().get_background_pixmap();
 		
-		weak Gdk.Window root = Gdk.get_default_root_window();
-			
-		int x, y, width, height, depth;
-		root.get_geometry(out x, out y, out width, out height, out depth);
+		/* create fullsize Pixbuf */
+		Gdk.Pixbuf ss = _wnck_gdk_pixbuf_get_from_pixmap(null,
+                                              			 p,
+                                              			 0, 0,
+                                              			 0, 0,
+                                              			 -1, -1);
 		
-		Gdk.Pixbuf ss = new Gdk.Pixbuf(Gdk.Colorspace.RGB,
-									   false,
-									   8,
-									   width,
-									   height);
-									   
+		/* created thumbnail Pixbuf */			   
 		Gdk.Pixbuf pb = new Gdk.Pixbuf(Gdk.Colorspace.RGB,
 									   false,
 									   8,
 									   WIDTH,
-									   (int)((double)WIDTH/(double)width * height));
+									   (int)((double)WIDTH/(double)screen_width * screen_height));
+
 		
-		Gdk.pixbuf_get_from_drawable(ss,
-									 root,
-									 root.get_colormap(),
-									 0, 0,
-									 0, 0,
-									 width, height);
-									 
+		/* resize */
 		double ratio = (double)pb.width / (double)ss.width;
 		ss.scale(pb,
 				 0, 0,
---
+++ applet/x-utils-c.c
@@ -0,0 +1,112 @@
+#include <glib.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <gdk/gdk.h>
+#include <gdk/gdkx.h>
+
+/* the following functions are copied from libwnck-2.24.1
+ * Copyright (C) 2001 Havoc Pennington
+ * Copyright (C) 2005-2007 Vincent Untz */
+
+GdkColormap*
+get_cmap (GdkPixmap *pixmap);
+
+GdkPixbuf*
+_wnck_gdk_pixbuf_get_from_pixmap (GdkPixbuf   *dest,
+                                  Pixmap       xpixmap,
+                                  int          src_x,
+                                  int          src_y,
+                                  int          dest_x,
+                                  int          dest_y,
+                                  int          width,
+                                  int          height);
+
+
+GdkPixbuf*
+_wnck_gdk_pixbuf_get_from_pixmap (GdkPixbuf   *dest,
+                                  Pixmap       xpixmap,
+                                  int          src_x,
+                                  int          src_y,
+                                  int          dest_x,
+                                  int          dest_y,
+                                  int          width,
+                                  int          height)
+{
+  GdkDrawable *drawable;
+  GdkPixbuf *retval;
+  GdkColormap *cmap;
+  
+  retval = NULL;
+  cmap = NULL;
+  
+  drawable = gdk_xid_table_lookup (xpixmap);
+
+  if (drawable)
+    g_object_ref (G_OBJECT (drawable));
+  else
+    drawable = gdk_pixmap_foreign_new (xpixmap);
+
+  if (drawable)
+    {
+      cmap = get_cmap (drawable);
+
+      /* GDK is supposed to do this but doesn't in GTK 2.0.2,
+       * fixed in 2.0.3
+       */
+      if (width < 0)
+        gdk_drawable_get_size (drawable, &width, NULL);
+      if (height < 0)
+        gdk_drawable_get_size (drawable, NULL, &height);
+
+      retval = gdk_pixbuf_get_from_drawable (dest,
+                                             drawable,
+                                             cmap,
+                                             src_x, src_y,
+                                             dest_x, dest_y,
+                                             width, height);
+    }
+
+  if (cmap)
+    g_object_unref (G_OBJECT (cmap));
+  if (drawable)
+    g_object_unref (G_OBJECT (drawable));
+
+  return retval;
+}
+
+GdkColormap*
+get_cmap (GdkPixmap *pixmap)
+{
+  GdkColormap *cmap;
+
+  cmap = gdk_drawable_get_colormap (pixmap);
+  if (cmap)
+    g_object_ref (G_OBJECT (cmap));
+
+  if (cmap == NULL)
+    {
+      if (gdk_drawable_get_depth (pixmap) == 1)
+        {
+          /* try null cmap */
+          cmap = NULL;
+        }
+      else
+        {
+          /* Try system cmap */
+          GdkScreen *screen = gdk_drawable_get_screen (GDK_DRAWABLE (pixmap));
+          cmap = gdk_screen_get_system_colormap (screen);
+          g_object_ref (G_OBJECT (cmap));
+        }
+    }
+
+  /* Be sure we aren't going to blow up due to visual mismatch */
+  if (cmap &&
+      (gdk_colormap_get_visual (cmap)->depth !=
+       gdk_drawable_get_depth (pixmap)))
+    {
+      g_object_unref (G_OBJECT (cmap));
+      cmap = NULL;
+    }
+  
+  return cmap;
+}
