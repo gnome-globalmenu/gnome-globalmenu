1559c4364aecf316c40bf7109c61fcfa6aaa2fcf
Yu Feng <rainwoodman@gmail.com>
2009-02-15 02:14:26 +0000

    same as previous commit.

--- libgnomenu/patch.sh
+++ libgnomenu/patch.sh
@@ -14,8 +14,8 @@ patch 's/gboolean include_internals;//g;s/include_internals = FALSE;//g' menuite
 patch 's/GtkCallback callback, void\* callback_target, void\* data/gboolean include_internals, GtkCallback callback, void* callback_target/g' menuitem.c menubar.c menubarbox.c label.c
 patch 's/callback, callback_target, data/include_internals, callback, callback_target/g' menuitem.c menubar.c
 
-if [ pkg-config --exists 'libwnck-1.0 < 2.20' ]; then
-patch '/static void _gnomenu_monitor_on_active_window_changed_wnck_screen_active_window_changed/s;WnckWindow* previous_window, ;;' monitor.c
+if pkg-config --exists 'libwnck-1.0 < 2.20' ; then
+patch '/static void _gnomenu_monitor_on_active_window_changed_wnck_screen_active_window_changed/s;WnckWindow\* previous_window, ;;' monitor.c
 patch '/gnomenu_monitor_on_active_window_changed/s;self, _sender, previous_window;self, _sender, NULL;' monitor.c
 fi;
 
