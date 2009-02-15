59718399ed37614eb4f10ba365dd18f137d8ed5b
Yu Feng <rainwoodman@gmail.com>
2009-02-15 02:24:40 +0000

    should use gtk_menu_item_remove_submenu instead of gtk_menu_item_set_submenu on Gtk 2.10

--- libgnomenu/patch.sh
+++ libgnomenu/patch.sh
@@ -18,5 +18,7 @@ if pkg-config --exists 'libwnck-1.0 < 2.20' ; then
 patch '/static void _gnomenu_monitor_on_active_window_changed_wnck_screen_active_window_changed/s;WnckWindow\* previous_window, ;;' monitor.c
 patch '/gnomenu_monitor_on_active_window_changed/s;self, _sender, previous_window;self, _sender, NULL;' monitor.c
 fi;
-
+if pkg-config --exists 'gtk+-2.0 < 2.12' ; then
+patch 's;gtk_menu_item_set_submenu ((GtkMenuItem\*) self, NULL);gtk_menu_item_remove_submenu ((GtkMenuItem*) self);' menuitem.c
+fi
 check_restore menuitem.c globalmenu.c monitor.c menubar.c menubarbox.c label.c
