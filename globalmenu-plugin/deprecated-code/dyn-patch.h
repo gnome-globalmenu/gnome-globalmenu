3460e42ab12cd7570dccc880dcd63f974b89abba
Yu Feng <rainwoodman@gmail.com>
2009-11-07 01:05:14 -0500

    Remove deprecated code.

--- globalmenu-plugin/deprecated-code/dyn-patch.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DYN_PATCH_H__
-#define __DYN_PATCH_H__
-#include <dyn-patch-utils.h>
-#include <dyn-patch-vfunc.h>
-
-void dyn_patch_init(void);
-void dyn_patch_uninit_vfuncs(void);
-void dyn_patch_uninit_final(void);
-#endif
