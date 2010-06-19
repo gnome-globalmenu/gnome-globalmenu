ff14c8976ca900922f095cabe1fa6d3833f8fd28
rainwoodman <rainwoodman@gmail.com>
2010-06-19 19:54:54 -0400

    better standalone applet (like a dragable panel)

--- tools/standalone.c
+++ tools/standalone.c
@@ -12,9 +12,11 @@ main (int argc, char **argv)
 
     menubar = gnomenu_global_menu_bar_new();
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-
-	gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(menubar));
-	gtk_widget_show_all(window);
+    gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_MENU);
+    gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+    gtk_window_set_accept_focus(GTK_WINDOW(window), FALSE);
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(menubar));
+    gtk_widget_show_all(window);
 
     gtk_main ();
     return 0;
